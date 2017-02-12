14cd047cd0708b78aecb8dbd32b372d708c23fb6
Jens Borch Christiansen <jens.borch@gmail.com>
2017-02-12 12:42:13 +0100

    Started adding plain json content processors and updated dependencies

---
+++ processing/src/main/java/dk/kontentsu/processing/JsonContent.java
@@ -0,0 +1,22 @@
+package dk.kontentsu.processing;
+
+/**
+ * Constants for JSON content.
+ *
+ * @author Jens Borch Christiansen
+ */
+public final class JsonContent {
+
+    public static final String JSON_COMPOSITION = "composition";
+    public static final String JSON_COMPOSITION_TYPE = "composition-type";
+    public static final String JSON_CONTENT = "content";
+    public static final String JSON_HREF = "href";
+    public static final String JSON_LINKS = "links";
+    public static final String JSON_LINK = "link";
+    public static final String JSON_METADATA = "metadata";
+    public static final String JSON_SELF_LINK = "self";
+
+    private JsonContent() {
+    }
+
+}
