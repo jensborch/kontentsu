953bd1c8c5f6b63d517f163a873b38c51344909e
Jens Borch Christiansen <jens.borch@gmail.com>
2017-02-21 21:56:19 +0100

    Started adding test for JSON parse

---
+++ processing/src/test/java/dk/kontentsu/processing/JsonParserTest.java
@@ -0,0 +1,53 @@
+package dk.kontentsu.processing;
+
+import dk.kontentsu.processing.HalJsonParser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import dk.kontentsu.model.Content;
+import dk.kontentsu.model.MimeType;
+import dk.kontentsu.model.internal.Metadata;
+import dk.kontentsu.model.internal.MetadataType;
+import dk.kontentsu.parsers.ContentParser;
+import dk.kontentsu.test.ContentTestData;
+import java.nio.charset.StandardCharsets;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.internal.util.reflection.Whitebox;
+
+/**
+ * Test for {@link HalJsonParser}
+ *
+ * @author Jens Borch Christiansen
+ */
+public class HalJsonParserTest {
+
+    private ContentTestData data;
+
+    @Before
+    public void setUp() {
+        data = new ContentTestData();
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testParse() throws Exception {
+        Content content = new Content(data.getFullPage(), StandardCharsets.UTF_8, MimeType.APPLICATION_HAL_JSON_TYPE);
+        HalJsonParser parser = new HalJsonParser();
+        Whitebox.setInternalState(parser, "content", content);
+
+        ContentParser.Results result = parser.parse();
+
+        assertEquals(7, result.getMetadata().size());
+        assertTrue(result.getMetadata().containsKey(new Metadata.Key(MetadataType.SEO, "author")));
+        assertEquals("Peter Nielsen", result.getMetadata().get(new Metadata.Key(MetadataType.SEO, "author")).getValue());
+        assertEquals(5, result.getLinks().size());
+
+    }
+
+}
