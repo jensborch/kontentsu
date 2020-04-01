fefcc5d415c22c1d9f934eb696da2e86cba55284
Jens Borch Christiansen <jens.borch@gmail.com>
2020-04-01 22:52:19 +0200

    Fixed Gradle issues

--- core/src/it/java/dk/kontentsu/repository/TermRepositoryIT.java
+++ core/src/it/java/dk/kontentsu/repository/TermRepositoryIT.java
@@ -2,7 +2,7 @@ package dk.kontentsu.repository;
 
 import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
